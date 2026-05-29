d3a39d394d7d19f32d70f35d2d4c322e9958d13e
Sufarwee Dunyamat <sufarwee@gmail.com>
2026-05-30 03:38:45 +0700

    Update site and base URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://sufarwee.github.io/myprofile/",
-  base: "/myprofile/",
+  site: "https://sufarwee.github.io",
+  base: "myprofile",
   output: "static",
   build: {
     inlineStylesheets: "auto",
