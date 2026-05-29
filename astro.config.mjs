1b8e7828d3cb7d571f28bb370421a48180aef74c
Sufarwee Dunyamat <sufarwee@gmail.com>
2026-05-30 03:39:10 +0700

    Fix base path in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://sufarwee.github.io",
-  base: "myprofile",
+  base: "/myprofile",
   output: "static",
   build: {
     inlineStylesheets: "auto",
